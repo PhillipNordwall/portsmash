2e47cba0e9f77a96b66d074e62c4b2fa538b6ab8
Phillip Nordwall <Phillip.Nordwall+github@gmail.com>
2018-11-08 14:10:04 -0800

    Update sync.sh

--- sync.sh
+++ sync.sh
@@ -4,8 +4,8 @@ OPENSSL=/usr/local/ssl/bin/openssl
 KEY_CURVE=secp384r1
 KEY_FILE=$KEY_CURVE.pem
 
-make clean
-make
+gmake clean
+gmake
 pkill spy
 rm -f pipe.fifo
 
@@ -21,16 +21,16 @@ mkfifo pipe.fifo
 # Victims: exactly one of these should active at runtime, so make sure exactly one is commented out.
 
 # Victim 1: start signing but it will be blocked
-taskset -c 7 $OPENSSL dgst -sha512 -sign $KEY_FILE -out data.sig pipe.fifo &
+cpuset -c -l 0 $OPENSSL dgst -sha512 -sign $KEY_FILE -out data.sig pipe.fifo &
 
 # Victim 2: start scalar multiplication but it will be blocked
-#taskset -c 7 ./ecc M 4 000084210000842100008421000084210000842100008421000084210000842100008421000084210000842100008421 &
+#cpuset -c -l 0 ./ecc M 4 000084210000842100008421000084210000842100008421000084210000842100008421000084210000842100008421 &
 
 sleep 0.1
 
 # Spy: must be on same physical core, but different logical core
 # start spying and generate the message to be signed
-taskset -c 3 ./spy
+cpuset -c -l 1 ./spy
 
 # wait to finish the signature/spying
 wait
